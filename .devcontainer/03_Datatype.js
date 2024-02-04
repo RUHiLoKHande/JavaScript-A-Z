8b542cb5f6f83be72f25a450f5801f7ce7effb03
RUHiLoKHande <146420996+RUHiLoKHande@users.noreply.github.com>
2024-02-04 14:45:12 +0000

    conversion of data type

---
+++ .devcontainer/03_Datatype.js
@@ -0,0 +1,27 @@
+/*
+let score=23;
+let score1='245abc';
+let name=null
+let Roll=undefined
+let pass=true
+
+
+let newscore=Number(score1)
+
+console.log(typeof score)
+console.log(typeof newscore)
+console.log(typeof name)
+console.log(newscore)
+console.log(typeof Roll)
+console.log(pass)
+
+let isloggeIN=1
+let booleanislogein=Boolean(isloggeIN)
+console.log(booleanislogein)
+*/
+
+let name=123
+let somestring=String(name)
+console.log(somestring)
+console.log(typeof somestring)
+
