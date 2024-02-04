0954c5159691484974ee2c724ea39e009ab03766
RUHiLoKHande <146420996+RUHiLoKHande@users.noreply.github.com>
2024-02-04 14:44:47 +0000

    configration

---
+++ .devcontainer/01_test.js
@@ -0,0 +1,11 @@
+const account_Id=1234
+let Name="Rushikesh Lokhande"
+var Password="2345"
+City="Pune"
+
+//account_Id=3213  we can not change the const varible;
+Name="Aryan Lokhande"
+Password="4321"
+City="Jaipur"
+
+console.table([Name,Password,City])
---
+++ .devcontainer/02_Datatype.js
@@ -0,0 +1,15 @@
+console.log(3+3)
+console.log("Ruhai")
+let login=false
+/*
+#Primitive datatye
+1.number=2 to power 53
+2.bigint
+3.String=""
+4.boolean=true/false
+5.null=standalone value
+6.undefine
+7.symbol=unique
+*/
+console.log(typeof "rushi")
+console.log(typeof login)
